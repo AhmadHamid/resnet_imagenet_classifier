7252959d5db1980afcdb6e68a348fb16ba2358d8
Ahmad Rzgar Hamid <arh@mmmi.sdu.dk>
2024-07-09 14:10:28 +0200

    Init

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+from flask import Flask, request
+from resnet50 import predict
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def root():
+  if (request.method == "GET"):
+    return '''
+      <!doctype html>
+      <title>Upload new File</title>
+      <h1>Upload new File</h1>
+      <form method=post enctype=multipart/form-data>
+        <input type=file name=file>
+        <input type=submit value=Upload>
+      </form>
+      '''
+  elif (request.method == "POST"):
+    file = request.files["file"]
+    file.save("/tmp/resnet_images/{}".format(file.filename))
+    start = time.time()
+    print(predict(file.filename))
+    print(time.time() - start)
+    return file.filename
+  else:
+    return "Unknown"
---
+++ resnet50.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras.applications.resnet_v2 import ResNet152V2
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet152V2(weights="imagenet")
+
+def predict(filename):
+  img_path = "/tmp/resnet_images/{}".format(filename)
+  img = image.load_img(img_path, target_size=(224, 224))
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+  x = preprocess_input(x)
+
+  preds = model.predict(x)
+  return decode_predictions(preds, top=3)
