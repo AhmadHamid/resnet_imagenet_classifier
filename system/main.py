3adfba1c67858fb33deceba27277991bdeb2f1cc
Ahmad Rzgar Hamid <arh@mmmi.sdu.dk>
2024-07-11 12:36:04 +0200

    Add metrics endpoint and calculate response times

--- system/main.py
+++ system/main.py
@@ -1,8 +1,26 @@
-import time
+import time, json, math
 from flask import Flask, request
 from resnet50 import predict
 
 app = Flask(__name__)
+
+response_times = []
+count = 1000
+pointer = 0
+
+def set_response_time(res_time: float) -> bool:
+  response_times.insert(pointer, res_time)
+  pointer = (pointer + 1) % count
+
+def get_response_time() -> float:
+  sum = 0.0
+  for res_time in response_times:
+    sum += res_time
+
+  if (len(response_times) > 0):
+    return sum / len(response_times)
+  else:
+    return -1.0
 
 @app.route("/", methods=["GET", "POST"])
 def root():
@@ -19,8 +37,20 @@ def root():
   elif (request.method == "POST"):
     file = request.files["file"]
     file.save("/{}".format(file.filename))
-    start = time.time()
+    start_time = time.time()
     print(predict(file.filename))
-    return str(time.time() - start)
+    total_time = time.time() - start_time
+
+    set_response_time(total_time)
+
+    return str(total_time)
   else:
     return "Unknown"
+    
+@app.route("/metrics")
+def metrics():
+  return json.dumps({
+    "cpu": "100",
+    "responseTime": get_response_time()
+    })
+# TODO: Response TIme, Accuracy, Recall, Precision, F1-Score
